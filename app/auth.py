1ae597faac506ade91b328732ed8ebad4d08c70a
Moritz Schirmer <68462312+mosstuff@users.noreply.github.com>
2025-07-14 11:43:14 +0200

    fix settings load

--- app/auth.py
+++ app/auth.py
@@ -1,12 +1,13 @@
-from app.config import Settings
+from app.config import reload_settings
 from fastapi.security.api_key import APIKeyHeader
 from fastapi import Security, HTTPException
 from starlette.status import HTTP_403_FORBIDDEN
 
 api_key_header = APIKeyHeader(name="access_token", auto_error=False)
+settings = reload_settings()
 
 async def get_api_key(api_key_header: str = Security(api_key_header)):
-    if api_key_header == Settings.secret_key:
+    if api_key_header == settings.secret_key:
         return api_key_header
     else:
         raise HTTPException(
